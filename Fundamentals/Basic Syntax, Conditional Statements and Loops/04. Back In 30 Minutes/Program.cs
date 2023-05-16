382c1f5d491577de945fcf38f5eb3c81d164ecba
Vladimir Bojinov <133802678+vladimirbojinov@users.noreply.github.com>
2023-05-16 21:23:39 +0300

    Add files via upload

---
+++ Fundamentals/Basic Syntax, Conditional Statements and Loops/04. Back In 30 Minutes/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _04._Back_In_30_Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hh = int.Parse(Console.ReadLine());
+            int mm = int.Parse(Console.ReadLine());
+
+            mm += 30;
+
+            if (mm >= 60)
+            {
+                hh++;
+                mm = mm - 60;
+            }
+            if (hh >= 24)
+            {
+                hh = 0;
+            }
+
+            Console.WriteLine($"{hh}:{mm:d2}");
+        }
+    }
+}
