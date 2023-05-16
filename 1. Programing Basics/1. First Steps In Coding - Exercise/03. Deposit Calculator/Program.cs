f5ecf32c0197dd59e5b2d4f314b2179cef786e5c
Vladimir Bojinov <133802678+vladimirbojinov@users.noreply.github.com>
2023-05-17 00:03:33 +0300

    Add files via upload

---
+++ 1. Programing Basics/1. First Steps In Coding - Exercise/03. Deposit Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _03._Deposit_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            double depositSum = double.Parse(Console.ReadLine());
+            int depositInMonths = int.Parse(Console.ReadLine());
+            double yearInterestPecentage = double.Parse(Console.ReadLine());
+            //calculations
+            double totalSum = depositSum + depositInMonths * ((depositSum * yearInterestPecentage / 100) / 12);
+            //output
+            Console.WriteLine(totalSum);
+        }
+    }
+}
