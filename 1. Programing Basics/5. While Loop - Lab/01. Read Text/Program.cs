064c6f4d2faa50e07489b907e631721719cbc202
Vladimir Bojinov <133802678+vladimirbojinov@users.noreply.github.com>
2023-05-17 23:05:53 +0300

    Add files via upload

---
+++ 1. Programing Basics/5. While Loop - Lab/01. Read Text/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _01._Read_Text
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string words = Console.ReadLine();
+
+                if (words == "Stop")
+                {
+                    break;
+                }
+
+                Console.WriteLine(words);
+            }
+        }
+    }
+}
---
+++ 1. Programing Basics/5. While Loop - Lab/05. Account Balance/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _05._Account_Balance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            double balance = 0.0;
+
+            while (input != "NoMoreMoney")
+            {
+                double amount = double.Parse(input);
+
+                if (amount < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+
+                balance += amount;
+
+                Console.WriteLine($"Increase: {amount:f2}");
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Total: {balance:f2}");
+        }
+    }
+}
