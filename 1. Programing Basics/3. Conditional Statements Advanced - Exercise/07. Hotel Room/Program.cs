e639e518af801ba1def4171f52f7f0c72469b31f
Vladimir Bojinov <133802678+vladimirbojinov@users.noreply.github.com>
2023-05-17 22:15:56 +0300

    Add files via upload

---
+++ 1. Programing Basics/3. Conditional Statements Advanced - Exercise/07. Hotel Room/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace _07._Hotel_Room
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string month = Console.ReadLine();
+            int night = int.Parse(Console.ReadLine());
+
+            double studioPrice = 0;
+            double apartmentPrice = 0;
+
+            switch (month)
+            {
+                case "May":
+                case "October": studioPrice = 50; apartmentPrice = 65; ; break;
+                case "June":
+                case "September": studioPrice = 75.2; apartmentPrice = 68.70; break;
+                case "July":
+                case "August": studioPrice = 76; apartmentPrice = 77; break;
+            }
+
+            if (night > 7 && night <= 14)
+            {
+                if (month == "May" || month == "October")
+                {
+                    studioPrice *= 0.95;
+                }
+            }
+            else if (night > 14)
+            {
+                if (month == "May" || month == "October")
+                {
+                    studioPrice *= 0.7;
+                }
+                else
+                {
+                    if (month == "June" || month == "September")
+                    {
+                        studioPrice *= 0.8;
+                    }
+
+                }
+                apartmentPrice *= 0.9;
+            }
+
+            double finalStudioPrice = studioPrice * night;
+            double finalApartamentPrice = apartmentPrice * night;
+
+            Console.WriteLine($"Apartment: {finalApartamentPrice:f2} lv.");
+            Console.WriteLine($"Studio: {finalStudioPrice:f2} lv.");
+        }
+    }
+}
---
+++ 1. Programing Basics/3. Conditional Statements Advanced - Exercise/09. Ski Trip/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+namespace _09._Ski_Trip
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine()) - 1;
+            string room = Console.ReadLine();
+            string review = Console.ReadLine();
+
+            double totalSum = 0;
+
+            switch (room)
+            {
+                case "room for one person": totalSum = 18.0;
+                    totalSum *= 1;
+                    break;
+                case "apartment": totalSum = 25.0;
+                    if (days < 10)
+                    {
+                        totalSum *= 0.7;
+                    }
+                    else if (days <= 15)
+                    {
+                        totalSum *= 0.65;
+                    }
+                    else if (days > 15)
+                    {
+                        totalSum *= 0.5;
+                    }
+                    break;
+                case "president apartment": totalSum = 35.0;
+                    if (days < 10)
+                    {
+                        totalSum *= 0.9;
+                    }
+                    else if (days <= 15)
+                    {
+                        totalSum *= 0.85;
+                    }
+                    else if (days > 15)
+                    {
+                        totalSum *= 0.8;
+                    }
+                    break;
+            }
+            
+            if (review == "positive")
+            {
+                totalSum *= 1.25;
+            }
+            else
+            {
+                totalSum *= 0.9;
+            }
+
+            double finalPrice = totalSum * days;
+
+            Console.WriteLine($"{finalPrice:f2}");
+        }
+    }
+}
