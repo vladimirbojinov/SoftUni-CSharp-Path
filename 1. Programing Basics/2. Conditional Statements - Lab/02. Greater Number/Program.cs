187f40dd2b72c599a4a355b28136ec75319e98f2
Vladimir Bojinov <133802678+vladimirbojinov@users.noreply.github.com>
2023-05-17 18:19:56 +0300

    Add files via upload

---
+++ 1. Programing Basics/2. Conditional Statements - Lab/02. Greater Number/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _02._Greater_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+
+            if (n1 > n2)
+            {
+                Console.WriteLine(n1);
+            }
+            else
+            {
+                Console.WriteLine(n2);
+            }
+        }
+    }
+}
