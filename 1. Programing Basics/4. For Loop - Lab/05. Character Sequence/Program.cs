80300cec3a8db331edaeee897b9ea7863beb84fd
Vladimir Bojinov <133802678+vladimirbojinov@users.noreply.github.com>
2023-05-17 22:31:39 +0300

    Add files via upload

---
+++ 1. Programing Basics/4. For Loop - Lab/05. Character Sequence/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _05._Character_Sequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string txt = Console.ReadLine();
+
+            for (int i = 0; i < txt.Length; i++)
+            {
+                Console.WriteLine(txt[i]);
+            }
+        }
+    }
+}
