3ee459cd225785bef83396baa3d446ea03451367
Vladimir Bojinov <vladimirbojinov05@gmail.com>
2023-05-28 14:14:02 +0300

    updating

--- 2. Fundamentals/3. Arrays - Exercise/8. Magic Sum/Program.cs
+++ 2. Fundamentals/3. Arrays - Exercise/8. Magic Sum/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] array1 = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            while (array1.Length > 1)
+            {
+                int[] condensed = new int[array1.Length - 1];
+                for (int i = 0; i < array1.Length - 1; i++)
+                {
+                    condensed[i] = array1[i] + array1[i + 1];
+                }
+                array1 = condensed;
+            }
+
+            Console.WriteLine(array1[0]);
         }
     }
 }
