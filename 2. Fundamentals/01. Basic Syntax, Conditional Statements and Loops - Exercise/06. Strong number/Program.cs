0085a5aef4d3c82765a1dc84f8dc33c198e12b8e
Vladimir Bojinov <vladimirbojinov05@gmail.com>
2023-07-25 02:12:28 +0300

    Update

---
+++ 2. Fundamentals/01. Basic Syntax, Conditional Statements and Loops - Exercise/06. Strong number/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _06._Strong_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string num = Console.ReadLine();
+
+            int sum = 1;
+            int finalNum = 0;
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                char currentCharacter = num[i];
+                string str = currentCharacter.ToString();
+                int curentNum = int.Parse(str);
+
+                for (int j = 1; j <= curentNum; j++)
+                {
+                    sum *= j;
+                }
+
+                finalNum += sum;
+                sum = 1;
+            }
+
+            int num2 = int.Parse(num);
+
+            if (finalNum == num2)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
