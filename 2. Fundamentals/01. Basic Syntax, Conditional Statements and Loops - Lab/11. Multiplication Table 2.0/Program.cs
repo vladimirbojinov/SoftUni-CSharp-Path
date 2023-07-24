0f55ce5bdae67f5bbecd2c7f2510a7dd5e9d5817
Vladimir Bojinov <vladimirbojinov05@gmail.com>
2023-07-25 02:05:37 +0300

    Update

---
+++ 2. Fundamentals/01. Basic Syntax, Conditional Statements and Loops - Lab/11. Multiplication Table 2.0/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _11._Multiplication_Table_2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+
+            if (n2 > 10)
+            {
+                Console.WriteLine($"{n} X {n2} = {n * n2}");
+            }
+
+            for (int i = n2; i <= 10; i++)
+            {
+                Console.WriteLine($"{n} X {i} = {n * i}");
+            }
+        }
+    }
+}
